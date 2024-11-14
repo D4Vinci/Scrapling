dac24c23e3d03d0346aaac722d7f5da14fbfe223
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-11-14 17:12:03 +0900

    chore: update window_chrome.js
    occured -> occurred

--- scrapling/engines/toolbelt/bypasses/window_chrome.js
+++ scrapling/engines/toolbelt/bypasses/window_chrome.js
@@ -196,7 +196,7 @@ const timingInfo = {
     },
     get firstPaintTime() {
         const fpEntry = performance.getEntriesByType('paint')[0] || {
-            startTime: timing.loadEventEnd / 1000, // Fallback if no navigation occured (`about:blank`)
+            startTime: timing.loadEventEnd / 1000, // Fallback if no navigation occurred (`about:blank`)
         };
         return toFixed(
             (fpEntry.startTime + performance.timeOrigin) / 1000,
