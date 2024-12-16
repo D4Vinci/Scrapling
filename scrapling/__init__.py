05c6eeb3870a6fb4750faa1f41fcdd5761c6c8f6
Karim shoair <D4Vinci@users.noreply.github.com>
2024-12-16 12:37:29 +0200

    fix: add AsyncFetcher to top-level shortcuts

--- scrapling/__init__.py
+++ scrapling/__init__.py
@@ -1,7 +1,7 @@
 # Declare top-level shortcuts
 from scrapling.core.custom_types import AttributesHandler, TextHandler
-from scrapling.fetchers import (CustomFetcher, Fetcher, PlayWrightFetcher,
-                                StealthyFetcher)
+from scrapling.fetchers import (AsyncFetcher, CustomFetcher, Fetcher,
+                                PlayWrightFetcher, StealthyFetcher)
 from scrapling.parser import Adaptor, Adaptors
 
 __author__ = "Karim Shoair (karim.shoair@pm.me)"
@@ -9,4 +9,4 @@ __version__ = "0.2.9"
 __copyright__ = "Copyright (c) 2024 Karim Shoair"
 
 
-__all__ = ['Adaptor', 'Fetcher', 'StealthyFetcher', 'PlayWrightFetcher']
+__all__ = ['Adaptor', 'Fetcher', 'AsyncFetcher', 'StealthyFetcher', 'PlayWrightFetcher']
--- scrapling/defaults.py
+++ scrapling/defaults.py
@@ -1,6 +1,7 @@
-from .fetchers import Fetcher, PlayWrightFetcher, StealthyFetcher
+from .fetchers import AsyncFetcher, Fetcher, PlayWrightFetcher, StealthyFetcher
 
 # If you are going to use Fetchers with the default settings, import them from this file instead for a cleaner looking code
 Fetcher = Fetcher()
+AsyncFetcher = AsyncFetcher()
 StealthyFetcher = StealthyFetcher()
 PlayWrightFetcher = PlayWrightFetcher()
