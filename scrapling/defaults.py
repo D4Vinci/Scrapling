1332be7e63c3f71115dd95634c5804c6d0e9f9fc
Karim shoair <D4Vinci@users.noreply.github.com>
2025-01-28 23:44:58 +0200

    fix(autocompletion): Fix the defaults import
    It runs autocompletion on some IDEs

--- scrapling/defaults.py
+++ scrapling/defaults.py
@@ -1,7 +1,10 @@
-from .fetchers import AsyncFetcher, Fetcher, PlayWrightFetcher, StealthyFetcher
+from .fetchers import AsyncFetcher as _AsyncFetcher
+from .fetchers import Fetcher as _Fetcher
+from .fetchers import PlayWrightFetcher as _PlayWrightFetcher
+from .fetchers import StealthyFetcher as _StealthyFetcher
 
 # If you are going to use Fetchers with the default settings, import them from this file instead for a cleaner looking code
-Fetcher = Fetcher()
-AsyncFetcher = AsyncFetcher()
-StealthyFetcher = StealthyFetcher()
-PlayWrightFetcher = PlayWrightFetcher()
+Fetcher = _Fetcher()
+AsyncFetcher = _AsyncFetcher()
+StealthyFetcher = _StealthyFetcher()
+PlayWrightFetcher = _PlayWrightFetcher()
