7aa083053b5909a0f2630db071d7f1f7774dac29
Karim shoair <D4Vinci@users.noreply.github.com>
2025-10-01 03:50:57 +0300

    test: change tests accordingly

--- tests/fetchers/async/test_camoufox_session.py
+++ tests/fetchers/async/test_camoufox_session.py
@@ -4,7 +4,7 @@ import asyncio
 
 import pytest_httpbin
 
-from scrapling.engines._browsers import AsyncStealthySession
+from scrapling.fetchers import AsyncStealthySession
 
 
 @pytest_httpbin.use_class_based_httpbin
