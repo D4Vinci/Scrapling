a54ceb333590fb3d0d67518b460af3c92dd17911
Karim shoair <D4Vinci@users.noreply.github.com>
2025-10-19 15:38:46 +0300

    build: Pumping up the deps and the version

--- scrapling/__init__.py
+++ scrapling/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Karim Shoair (karim.shoair@pm.me)"
-__version__ = "0.3.7"
+__version__ = "0.3.8"
 __copyright__ = "Copyright (c) 2024 Karim Shoair"
 
 from typing import Any, TYPE_CHECKING
