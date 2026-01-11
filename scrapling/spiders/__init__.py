f2c621696803c29b3f43a8fe18fe643ba38b7910
Karim shoair <D4Vinci@users.noreply.github.com>
2026-01-12 01:21:26 +0200

    style: remove unwanted import from spiders main init file

--- scrapling/spiders/__init__.py
+++ scrapling/spiders/__init__.py
@@ -1,18 +1,16 @@
-from .spider import Spider, SessionConfigurationError, LogCounterHandler
 from .request import Request
-from .result import CrawlStats, CrawlResult
+from .result import CrawlResult
+from .scheduler import Scheduler
 from .engine import CrawlerEngine
 from .session import SessionManager
-from .scheduler import Scheduler
+from .spider import Spider, SessionConfigurationError
 from scrapling.engines.toolbelt.custom import Response
 
 __all__ = [
     "Spider",
     "SessionConfigurationError",
-    "LogCounterHandler",
     "Request",
     "CrawlerEngine",
-    "CrawlStats",
     "CrawlResult",
     "SessionManager",
     "Scheduler",
